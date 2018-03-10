f00848cedc9eb2b8b14e677348c2a094f6c598db
Dan George <dgeorge83616@yahoo.com>
2018-03-09 17:38:25 -0800

    Addressed pylint complaints

--- tic-tac-toe/ttt.py
+++ tic-tac-toe/ttt.py
@@ -1,118 +1,133 @@
-#!python
+'''
+Tic-Tac-Toe Project
+Dan George
+'''
+from random import sample
 from IPython.display import clear_output
-import sys
-from random import *
 
-next_player = {'X':'O', 'O':'X'}
+NEXT_PLAYER = {'X':'O', 'O':'X'}
 
 def create_board():
+    ''' Create a board '''
     row = [' ']*3
     return [row, row.copy(), row.copy()]
 
 def copy_board(board):
+    ''' Make a copy of the board '''
     new_board = []
-    for r in board:
-        new_board.append(r.copy())
+    for row in board:
+        new_board.append(row.copy())
     return new_board
 
 def display_board(board):
-     clear_output()
-     print("")
-     row = board[0]
-     print(f" {row[0]} | {row[1]} | {row[2]} ")
-     print(f"---+---+---")
-     row = board[1]
-     print(f" {row[0]} | {row[1]} | {row[2]} ")
-     print(f"---+---+---")
-     row = board[2]
-     print(f" {row[0]} | {row[1]} | {row[2]} ")
-     return board
+    ''' Draw the board '''
+    clear_output()
+    print("")
+    row = board[0]
+    print(f" {row[0]} | {row[1]} | {row[2]} ")
+    print(f"---+---+---")
+    row = board[1]
+    print(f" {row[0]} | {row[1]} | {row[2]} ")
+    print(f"---+---+---")
+    row = board[2]
+    print(f" {row[0]} | {row[1]} | {row[2]} ")
+    return board
 
 def is_winner(board):
-    for r in board:
-        if r[0] == r[1] == r[2] != ' ':
+    ''' Check for winner '''
+    for row in board:
+        if row[0] == row[1] == row[2] != ' ':
             return True
-    for c in [0, 1, 2]:
-        if board[0][c] == board[1][c] == board[2][c] != ' ':
+    for column in [0, 1, 2]:
+        if board[0][column] == board[1][column] == board[2][column] != ' ':
             return True
-    if board[0][0] == board[1][1] == board[2][2] != ' ' or board[2][0] == board[1][1] == board[0][2] != ' ':
+    if board[0][0] == board[1][1] == board[2][2] != ' ' \
+            or board[2][0] == board[1][1] == board[0][2] != ' ':
         return True
     return False
 
-def is_draw(board, player):
+def is_draw(board, a_player):
+    ''' Determine if the move made by player has forced a draw '''
     if is_winner(board):
         return False
-    player = next_player[player]
-    for r in range(0, len(board)):
-        for c in range(0, len(board[r])):
-            if board[r][c] == ' ':
+    a_player = NEXT_PLAYER[a_player]
+    for row, _ in enumerate(board):
+        for column, cell_value in row:
+            if cell_value == ' ':
                 new_board = copy_board(board)
-                set_cell(new_board, player, (r,c))
-                #display_board(new_board)
-                if not is_draw(new_board, player):
-                    return False;
+                set_cell(new_board, a_player, (row, column))
+                if not is_draw(new_board, a_player):
+                    return False
     return True
 
-def get_num(player):
-    c = input(f"           Player {player} choose a cell (number 1-9, or q): ").lower()
-    while not c in ['1', '2', '3', '4', '5', '6', '7', '8', '9', 'q']:
-        c = input(f" Oops! Choose a cell (number 1-9, or q): ").lower()
-    return c
+def get_num(a_player):
+    ''' Prompt the a_player until valid input '''
+    char = input(f"          Player {a_player} choose a cell (number 1-9, or q): ").lower()
+    while not char in ['1', '2', '3', '4', '5', '6', '7', '8', '9', 'q']:
+        char = input(f" Oops! Choose a cell (number 1-9, or q): ").lower()
+    return char
 
 def validate_cell(board, cell):
+    ''' Validates that the cell is not already taken '''
     return board[cell[0]][cell[1]] == ' '
 
-def get_cell(board, player):
-    num = get_num(player)
+def get_cell(board, a_player):
+    '''
+    Prompts the a_player for a cell and return it as a (x, y) tuple
+    '''
+    num = get_num(a_player)
     while num != 'q':
-        cell = [(2,0),(2,1),(2,2),
-                (1,0),(1,1),(1,2),
-                (0,0),(0,1),(0,2)][int(num)-1]
+        cell = [(2, 0), (2, 1), (2, 2),
+                (1, 0), (1, 1), (1, 2),
+                (0, 0), (0, 1), (0, 2)][int(num)-1]
         if validate_cell(board, cell):
             return cell
         else:
             print("Oops: That cell is already taken")
-    else:
-        return 'q'
+    return 'q'
 
-def set_cell(board, player, cell):
-    board[cell[0]][cell[1]] = player
+def set_cell(board, a_player, cell):
+    '''
+    Set the symbol passed in a_player into the cell argument
+    '''
+    board[cell[0]][cell[1]] = a_player
     return board
 
-def play(player):
+def play(n_player):
+    '''
+    Play a game using n_player arg as first mover
+    '''
     board = create_board()
     display_board(board)
-    quit = False
-    while not quit:
-        cell = get_cell(board, player)
+    while True:
+        cell = get_cell(board, n_player)
         if cell == 'q':
-            quit = True
-            continue
-        set_cell(board, player, cell)
+            print(f"\nPlayer {n_player} quit the game")
+            break
+        set_cell(board, n_player, cell)
         display_board(board)
         if is_winner(board):
-            print(f"\n{player} is the Winner!")
+            print(f"\n{n_player} is the Winner!")
             return
-        elif is_draw(board, player):
+        elif is_draw(board, n_player):
             print("\nDraw")
             return
-        player = next_player[player]
-    else:
-        print(f"\nPlayer {player} quit the game")
+        n_player = NEXT_PLAYER[n_player]
 
-play_again = True
-while play_again:
-    player = sample(next_player.keys(), 1)[0]
-    play(player)
-    no_choice = True
-    choice = ''
-    while no_choice:
-        choice = input("\nPlay again? ([y]/n): ")
-        if not choice.lower() in ['y', '', 'n', 'q']:
-            continue
-        no_choice = False
-    else:
+def session():
+    ''' Runs games until players decided to stop playing '''
+    play_again = True
+    while play_again:
+        player = sample(NEXT_PLAYER.keys(), 1)[0]
+        play(player)
+        no_choice = True
+        choice = ''
+        while no_choice:
+            choice = input("\nPlay again? ([y]/n): ")
+            if not choice.lower() in ['y', '', 'n', 'q']:
+                continue
+            no_choice = False
         play_again = choice in ['y', '']
-else:
     print("\nPlay again soon! Bye!")
 
+session()
