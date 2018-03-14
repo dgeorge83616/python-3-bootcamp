31cbfda7bea7002412335a2a17f31d04bc53c832
Dan George <dgeorge83616@yahoo.com>
2018-03-13 19:41:45 -0700

    Basic game working sans payout. Lots of possible refactoring, particularly to separate VC from M. Lots more tests could be added and test_play_game needs to be made deterministic (stacked deck).

--- blackjack/blackjack.py
+++ blackjack/blackjack.py
@@ -1,101 +1,199 @@
 #!python
-
-import random
-
 '''
 BLACKJACK GAME
 '''
+from random import shuffle
+
 SUITS = ['clubs', 'diamonds', 'hearts', 'spades']
-NAMES = ['Jack', 'Queen', 'King', 'Ace']
+FACE_CARDS = ['J', 'Q', 'K']
+NUMS = ['2', '3', '4', '5', '6', '7', '8', '9', '10']
 
-class Card:
-    '''
-    SIMPLE STRUCTURE FOR A CARD
-    '''
-    def __init__(self, suit, name):
-        self.suit = suit
-        self.name = name
-
-    def __str__(self):
-        return f"{self.suit}, {self.name}"
-
-    def __eq__(self, value):
-        return self.suit == value.suit and self.name == value.name
-
-    def get_name(self):
-        ''' Return just the name of this card '''
-        return self.name
-
-    def get_suit(self):
-        ''' Return just the suit of this card '''
-        return self.suit
-
-MARKER = Card("MARKER", "MARKER")
-
-class Deck(list):
-    '''
-    DECK
-    '''
+class Prompter:
+    ''' As named '''
     def __init__(self):
         pass
 
-    def fill_deck():
-        ''' Put cards into the deck '''
-        for suit in SUITS:
-            for i in range(2, 11):
-                self.append(Card(suit, str(i)))
-            for name in NAMES:
-                self.append(Card(suit, name))
+    def prompt(self, prompt):
+        ''' simple promt using stdin '''
+        return input(prompt)
 
-    def take_next(self):
-        ''' Take a card from the "top" of the deck '''
-        return self.pop(0)
+class Pack(list):
+    ''' Pack of cards '''
+    def __init__(self, num_decks):
+        full_suit = NUMS + FACE_CARDS + ['A']
+        full_deck = full_suit*len(SUITS)
+        super().__init__(full_deck*num_decks)
 
-    def take_all(self):
-        ''' Take all the cards out of the deck '''
-        cards = self.copy()
-        self.clear()
-        return cards
+def card_value(card):
+    ''' As named '''
+    if card in FACE_CARDS:
+        return 10
+    if card == 'A':
+        return 11
+    return int(card)
 
-    def shuffle(self):
+class Hand(list):
+    ''' As named '''
 
-
-class Pack(Deck):
-    ''' Holds the decks that will be used for the table '''
-
-    def __init__(self, decks):
-        for deck in decks:
-            self.cards += deck.take_all()
-
-    def __len__(self):
-        '''
-        Return the number of cards still in the pack
-        '''
-        return len(self.cards)
-
-    def insert_marker(self, position):
-        ''' As named '''
-        self.cards.insert(position, MARKER)
-
-class Hand:
-    pass
+    def val(self):
+        ''' Return the sum of the cards '''
+        val = 0
+        for card in self:
+            val += card_value(card)
+            if val > 21 and 'A' in self:
+                self[self.index('A')] = '1'
+                val -= 10
+        return val
 
 class Player:
-    pass
+    ''' Represents a player in the game '''
 
-class Dealer:
-    pass
+    def __init__(self, name, balance, prompter):
+        self.name = name
+        self.balance = balance
+        self.prompter = prompter
+        self.wager = 0
+        self.hand = Hand()
 
+    def __str__(self):
+        return f"{self.name}, {self.balance}, {self.wager}, {self.hand}"
+
+    def __repr__(self):
+        return self.name
+
+    def __eq__(self, other):
+        return self.name == other.name
+
+    def bet(self, min_wager, max_wager):
+        ''' As named, return True if made a bet. '''
+        if self.balance < min_wager:
+            return False
+        max_wager = min(self.balance, max_wager)
+        while True:
+            wager = str(self.prompter.prompt(f"{self.name}, make bet: "))
+            if wager.lower() == 'p':
+                return False
+            if wager.lower() == '$':
+                print(f"Bank balance: ${self.balance}")
+                continue
+            wager = int(wager)
+            if min_wager <= wager and wager <=  max_wager:
+                break
+        self.wager = wager
+        self.balance -= self.wager
+        return True
+
+    def hit(self, card):
+        self.hand += [str(card)];
+        return self.hand.val()
+
+    def play(self, cards):
+        ''' play till stand or busted '''
+        while True:
+            answer = str(self.prompter.prompt(f"{self.name}, take a card? (y/n): "))
+            if answer.lower() != 'y':
+                break
+            elif self.hit(cards.pop()) > 21:
+                break
+
+class Game:
+    ''' As named '''
+
+    def __init__(self, dealer, pack, wager_limits, players):
+        self.dealer = dealer
+        self.pack = pack
+        self.min_bet = wager_limits[0]
+        self.max_bet = wager_limits[1]
+        self.players = players
+        self.winners = []
+        self.inactive_players = []
+        self.pushes = []
+
+    def remove_inactive_players(self):
+        for player in self.inactive_players:
+            if player in self.players:
+                self.players.remove(player)
+
+    def take_bets(self):
+        ''' As named and remove non-betters '''
+        # take bets
+        for player in self.players:
+            if not player.bet(self.min_bet, self.max_bet):
+                self.inactive_players.append(player)
+        # remove non-bettors
+        self.remove_inactive_players()
+
+    def deal_cards(self):
+        ''' As named and return winners list, which may be empty '''
+        for player in self.players:
+            player.hit(self.pack.pop())
+        self.dealer.hit(self.pack.pop())
+        for player in self.players:
+            hand = player.hit(self.pack.pop())
+            if hand > 21:
+                self.inactive_players.append(player)
+            elif hand == 21:
+                # Game over - player wins
+                self.winners = [player]
+                break
+        # remove busted players
+        self.remove_inactive_players()
+
+        if self.winners:
+            return bool(self.winners)
+
+        hand = self.dealer.hit(self.pack.pop())
+        if hand > 21:
+            # Game over players win
+            self.winners = self.players
+        elif hand == 21:
+            # Game over
+            self.winners.append(self.dealer)
+        return bool(self.winners)
+
+    def payout(self):
+        pass
+
+    def play(self):
+        ''' main processing loop '''
+        shuffle(self.pack)
+
+        self.take_bets()
+        if not self.players:
+            return
+
+        if self.deal_cards():
+            return
+        print(f"Players:")
+        for player in self.players:
+            print(f"         {player}")
+        if int(card_value(self.dealer.hand[1])) >= 10:
+            dealer_shows = self.dealer.hand
+        else:
+            dealer_shows = ['X', self.dealer.hand[1]]
+        print(f"Dealer: {dealer_shows}")
+
+        for player in self.players:
+            player.play(self.pack)
+            if player.hand.val() > 21:
+                self.inactive_players.append(player)
+            else:
+                max_hand = max(max_hand, player.hand.val())
+        self.remove_inactive_players()
+
+        while self.dealer.hand.val() < 17:
+            self.dealer.hit(self.pack.pop())
+        if self.dealer.hand.val() > 21:
+            self.winners = self.players
+            return
+
+        for player in self.players:
+            if player.hand.val() >  self.dealer.hand.val():
+                self.winners.append(player)
+            if player.hand.val() == self.dealer.hand.val():
+                self.pushes.append(player)
+
+        self.payout()
 
 if __name__ == "__main__":
-    DECK = Deck()
-    print(f"{DECK}")
-    CARD = Card('clubs', '2')
-    card = DECK.take_next()
-    print(f"{card}")
-    if card == CARD:
-        print("equal")
-    else:
-        print("not equal")
-    print(f"{DECK}")
-
+    pass
--- blackjack/test_blackjack.py
+++ blackjack/test_blackjack.py
@@ -4,30 +4,77 @@ Tests for blackjack module
 '''
 
 import unittest
-from blackjack import Deck, Card, Pack
+from blackjack import Pack, Game, Player
 
-class TestDeck(unittest.TestCase):
+class TestPlayer(unittest.TestCase):
 
-    def test_take_next(self):
-        deck = Deck()
-        self.assertEqual(len(deck), 52)
-        card = deck.take_next()
-        self.assertEqual(len(deck), 51)
-        self.assertEqual(card, Card('clubs', '2'))
+    class InputMock:
+        ''' mock prompter '''
+        def __init__(self, inputs):
+            self.inputs = inputs
 
-    def test_take_all(self):
-        deck = Deck()
-        cards = deck.take_all()
-        self.assertEqual(len(deck), 0)
-        self.assertEqual(len(cards), 52)
+        def prompt(self, name):
+            ''' just returns the next test response '''
+            return self.inputs.pop(0)
+
+    def test_init(self):
+        print(f"{self.test_init}")
+        player = Player('Jose', 500, TestPlayer.InputMock(list([])))
+        print(f"Initialized player: {player}")
+
+    def test_bet(self):
+        print(f"{self.test_bet}")
+        prompter = TestPlayer.InputMock([10, 100, 101, 50, 9, 11, 'p'])
+        player = Player('Jose', 500, prompter)
+        player.bet(10, 100)
+        self.assertEqual(player.wager, 10)
+        player.bet(10, 100)
+        self.assertEqual(player.wager, 100)
+        player.bet(10, 100)
+        self.assertEqual(player.wager, 50)
+        player.bet(10, 100)
+        self.assertEqual(player.wager, 11)
+        self.assertFalse(player.bet(10, 100))
+
+    def test_play(self):
+        print(f"{self.test_play}")
+        prompter = TestPlayer.InputMock(['y', 'y', 'y'])
+        player = Player('Jose', 500, prompter)
+        cards = ['10', '10', '10']
+        player.play(cards)
+        print(f"Hand = {player.hand}")
+        self.assertTrue(player.hand)
+        self.assertTrue(player.hand.val() > 21)
 
 class TestPack(unittest.TestCase):
 
     def test_init(self):
-        num_decks = 4;
-        deck = Deck()
-        pack = Pack([deck.copy()]*num_decks)
-        self.assertEqual(len(pack), num_decks*len(deck))
-        
+        print(f"{self.test_init}")
+        pack = Pack(1)
+        #print(pack)
+        self.assertEqual(len(pack), 52)
+        pack = Pack(4)
+        #print(pack)
+        self.assertEqual(len(pack), 4*52)
+
+class TestBlackjack(unittest.TestCase):
+
+    def test_play_game(self):
+        print(f"{self.test_play_game}")
+        max_draws = ['y']*10
+        player_inputs = [10] + max_draws
+        game = Game(Player('Dealer', 0, TestPlayer.InputMock(max_draws.copy())),\
+                Pack(1), (10, 100),\
+                [Player('Dan', 100, TestPlayer.InputMock(player_inputs.copy())),\
+                Player('Gayle', 500, TestPlayer.InputMock(player_inputs.copy())),\
+                Player('Jose', 350, TestPlayer.InputMock(player_inputs.copy()))])
+        game.play()
+        if game.winners:
+            print("Winners:")
+            for winner in game.winners:
+                print(f"       {winner.name}, {winner.hand}")
+        print(f"pushes: {game.pushes}")
+        print(f"busts: {game.inactive_players}")
+
 if __name__ == "__main__":
     unittest.main()
